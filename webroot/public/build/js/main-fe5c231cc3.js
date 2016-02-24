e647adc3af2f49e93f049cd2f3d20fdc526325d1
Audrius <aubuka@gmail.com>
2016-02-24 00:46:18 +0000

    add some comments

--- webroot/public/build/js/main-fe5c231cc3.js
+++ webroot/public/build/js/main-fe5c231cc3.js
@@ -61,18 +61,32 @@ var AppView = exports.AppView = function (_View) {
 }(View);
 
 },{}],2:[function(require,module,exports){
-"use strict";
+'use strict';
 
-var _app = require("./app");
+var _app = require('./app');
 
 /**
  * Document ready event
  */
-document.addEventListener("DOMContentLoaded", function (event) {
+$(function () {
   new _app.AppView();
 }); /**
      * ES6
-     * ref: http://tastejs.com/todomvc-backbone-es6/
+     * ref:
+     * http://tastejs.com/todomvc-backbone-es6/
+     * https://github.com/tastejs/todomvc-backbone-es6/blob/gh-pages/js/app.js
+     * https://github.com/tastejs/todomvc-backbone-es6/blob/gh-pages/js/todo-app.js
+     * Browserify: https://github.com/substack/node-browserify#usage
+     * Image lazyload:
+     * http://luis-almeida.github.io/unveil/
+     * http://stackoverflow.com/questions/18366550/using-lazy-loading-with-backbone
+     *
+     * Backbone tutorial:
+     * https://github.com/amejiarosario/Backbone-tutorial/blob/master/backbone-tutorial.html
+     * http://adrianmejia.com/blog/2012/09/11/backbone-dot-js-for-absolute-beginners-getting-started/
+     *
+     * Soundcloud JS SDK
+     * https://developers.soundcloud.com/docs/api/sdks
      */
 
 },{"./app":1}]},{},[2]);
--- webroot/resources/assets/js/main.js
+++ webroot/resources/assets/js/main.js
@@ -1,6 +1,20 @@
 /**
  * ES6
- * ref: http://tastejs.com/todomvc-backbone-es6/
+ * ref:
+ * http://tastejs.com/todomvc-backbone-es6/
+ * https://github.com/tastejs/todomvc-backbone-es6/blob/gh-pages/js/app.js
+ * https://github.com/tastejs/todomvc-backbone-es6/blob/gh-pages/js/todo-app.js
+ * Browserify: https://github.com/substack/node-browserify#usage
+ * Image lazyload:
+ * http://luis-almeida.github.io/unveil/
+ * http://stackoverflow.com/questions/18366550/using-lazy-loading-with-backbone
+ *
+ * Backbone tutorial:
+ * https://github.com/amejiarosario/Backbone-tutorial/blob/master/backbone-tutorial.html
+ * http://adrianmejia.com/blog/2012/09/11/backbone-dot-js-for-absolute-beginners-getting-started/
+ *
+ * Soundcloud JS SDK
+ * https://developers.soundcloud.com/docs/api/sdks
  */
 
 import {AppView} from './app';
@@ -8,6 +22,6 @@ import {AppView} from './app';
 /**
  * Document ready event
  */
-document.addEventListener("DOMContentLoaded", function(event) {
+$(() => {
   new AppView();
 });

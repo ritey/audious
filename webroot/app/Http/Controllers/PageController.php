a4955e4f5897dfccbb1337bfdb1803d4e1b45b39
Audrius <aubuka@gmail.com>
2016-05-10 23:01:25 +0100

    remove soundcloud facade

--- webroot/app/Http/Controllers/PageController.php
+++ webroot/app/Http/Controllers/PageController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-use Njasm\Soundcloud\SoundcloudFacade;
 use App\Repositories\SoundcloudRepository;
 use App\Repositories\GoogleRepository;
 use App\Repositories\SpotifyRepository;
